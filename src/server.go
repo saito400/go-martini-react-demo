dbc559488ad19c08bcd1ad7c7696e60fa861bcd5
saito400 <ken.s.530000@gmail.com>
2015-03-28 15:27:40 +0900

    c

--- src/server.go
+++ src/server.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	//	"fmt"
 	"github.com/go-martini/martini"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
 )
 
 func main() {
+
+	log.Println("start")
+
+	os.Remove("./foo.db")
+
 	m := martini.Classic()
 	m.Get("/", func() string {
 		return "Hello world!"
